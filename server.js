4df2087901c1a078f35254f6b0d7c6ed7cf0239b
Samuel oshiobughie <Cedar@MACs-MacBook-Pro.local>
2022-04-15 23:53:44 +0100

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-defined-46056',
-        user : 'Cedar',
-        password : '',
-        database : 'vehicle-recognition'
+        host : process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
